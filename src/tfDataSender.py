0c12c52774b9a633edc262f905c8680f9acfa01e
Daniel <ddazaj@uninorte.edu.co>
2019-08-21 11:37:22 -0500

    fix tf ports

--- src/tfDataSender.py
+++ src/tfDataSender.py
@@ -16,7 +16,7 @@ def convertToSend(data):
 def callback(msg):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = convertToSend(msg)
-    sock.sendto(data, (IP, 4003))
+    sock.sendto(data, (IP, 4004))
     
 def listener():
     rospy.init_node('tfTransmiter', anonymous=True)
--- src/tfStaticDataSender.py
+++ src/tfStaticDataSender.py
@@ -16,7 +16,7 @@ def convertToSend(data):
 def callback(msg):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = convertToSend(msg)
-    sock.sendto(data, (IP, 4003))
+    sock.sendto(data, (IP, 4005))
     
 def listener():
     rospy.init_node('tfStaticTransmiter', anonymous=True)
